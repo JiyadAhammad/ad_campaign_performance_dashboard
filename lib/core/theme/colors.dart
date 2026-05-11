a189c559e08020a10d8056d4626b9a3cf16b514d
JiyadAhammad <jiyadahammad99@gmail.com>
2026-05-12 00:31:24 +0530

    style(ui): add central colors.dart for app colors

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // --- Palette ---
+  static const Color primary = Color(0xFF1CB4BF);
+  static const MaterialColor blue = Colors.blue;
+  static const Color darkBg = Color(0xFF0F1115);
+  static const Color darkCard = Color(0xFF1B1A1E);
+  static const Color lightBg = Color(0xFFF5F7FA);
+  static const Color lightCard = Colors.white;
+
+  // Status Colors (Shared)
+  static const Color success = Colors.green;
+  static const Color warning = Colors.orange;
+  static const Color error = Colors.red;
+  static const Color muted = Colors.grey;
+
+  // Light Bg for snackbar
+  static const Color successBg = Color(0xFF2ECC71); // light green
+  static const Color errorBg = Color(0xFFE74C3C); // light red
+  static const Color warningBg = Color(0xFFFFA726); // light orange
+  static const Color infoBg = Color(0xFF42A5F5); // light blue
+
+  // Hint/Placeholder Colors
+  static const Color darkHint = Color(0xDADADCCC); //  grey for dark theme
+  static const Color lightHint = Color(0xFFB0B4BA); //  grey for light theme
+}
