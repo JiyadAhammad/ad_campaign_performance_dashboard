366cf11022db6f4f179c33cce91dcd6a42f3041a
JiyadAhammad <jiyadahammad99@gmail.com>
2026-05-12 00:16:41 +0530

    chore(dev): update API base URL for development

---
+++ lib/core/app_config/config.dart
@@ -0,0 +1,16 @@
+import '../environment/env.dart';
+
+class AppConfig {
+  static const String devBaseUrl =
+      'https://e5eb0d84-2b7e-4c32-98b9-233668b4e189.mock.pstmn.io/v1';
+  // TODO : Update Production url here
+  static const String prodBaseUrl = 'Update Production url here';
+
+  static String get baseUrl {
+    if (Environment.current == AppEnvironment.dev) {
+      return devBaseUrl;
+    } else {
+      return prodBaseUrl;
+    }
+  }
+}
