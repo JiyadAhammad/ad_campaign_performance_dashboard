c212ae212167dc4447acd95387ee902c2953bcf8
JiyadAhammad <jiyadahammad99@gmail.com>
2026-05-14 00:17:22 +0530

    feat(ui): complete static UI implementation

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'core/environment/env.dart';
 import 'core/theme/theme.dart';
-import 'features/campaign_details/presentation/pages/campaign_details.dart';
 import 'features/root_navigation/root_navigation.dart';
 
 class MainApp extends StatelessWidget {
@@ -33,7 +32,7 @@ class _MyApp extends StatelessWidget {
           themeMode: mode,
           theme: AppTheme.lightTheme,
           darkTheme: AppTheme.darkTheme,
-          home: CampaignDetailScreen(),
+          home: RootNavigation(),
         );
       },
     );
